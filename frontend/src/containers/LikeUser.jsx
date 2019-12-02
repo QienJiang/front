1beb6dd5f339ea70251d8a120cb5425c50a68bdf
QienJiang <a804242681lrm@gmail.com>
2019-12-02 12:40:58 -0500

    xxx
    xxx

--- frontend/src/containers/LikeUser.jsx
+++ frontend/src/containers/LikeUser.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import {
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  Checkbox
+} from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import axios from "axios";
 import "./Login.css";
@@ -9,7 +14,8 @@ class LikeUser extends Component {
     super(props);
     this.state = {
       loadList: [],
-      username: "",
+      id: "",
+      like: true,
       isLoading: false,
       data: []
     };
@@ -17,30 +23,32 @@ class LikeUser extends Component {
 
   validateForm() {}
   validatecontent() {}
-  handleChange = (event) => {
+  handleChange = event => {
     this.setState({
       [event.target.id]: event.target.value
     });
   };
 
-  handlePostTwitte = async (event) => {
+  handlePostTwitte = async event => {
     event.preventDefault();
     this.setState({ isLoading: true });
-    const { username } = this.state;
-
+    const { id, like } = this.state;
+    let data = JSON.stringify({
+      id: id,
+      like: like
+    });
     axios
-      .get("http://130.245.168.66/user/" + username + "/posts", {
+      .post("http://130.245.168.66/item/" + id + "/like", data, {
         headers: { "Content-Type": "application/json;charset=UTF-8" }
       })
-      .then((result) => {
-        console.log("results", result.data.items);
-        this.setState({ data: result.data.items });
+      .then(result => {
+        console.log("like sucess");
         this.setState({ isLoading: false });
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
         console.log("fail");
-        alert("invalid childType");
+        alert("already like this item or item does not exist");
         this.setState({ isLoading: false });
       });
   };
@@ -48,7 +56,7 @@ class LikeUser extends Component {
   displayResult() {
     const { data } = this.state;
     let i = 1;
-    const listItems = data.map((number) => (
+    const listItems = data.map(number => (
       <ul>
         <li key={i.toString()}>
           id {i++} : {number}
@@ -57,36 +65,44 @@ class LikeUser extends Component {
     ));
     return <ul>{listItems}</ul>;
   }
-
+  handleLike = event => {
+    console.log("value", this.state.like);
+    this.setState({
+      like: event.target.checked
+    });
+    console.log("value change to", this.state.like);
+  };
   render() {
     const { data } = this.state;
     return (
       <div className="Login">
         <form onSubmit={this.handlePostTwitte}>
-          <FormGroup controlId="username">
-            <ControlLabel>username</ControlLabel>
+          <FormGroup controlId="id">
+            <ControlLabel>id</ControlLabel>
             <FormControl
               autoFocus
               type="text"
-              value={this.state.username}
+              value={this.state.id}
               onChange={this.handleChange}
             />{" "}
           </FormGroup>
-
+          <label>
+            like
+            <Checkbox
+              name="like"
+              checked={this.state.like}
+              onChange={this.handleLike}
+            ></Checkbox>
+          </label>
           <LoaderButton
             block
             bsSize="large"
             type="submit"
             isLoading={this.state.isLoading}
-            text="Search"
-            loadingText="Searching ..."
+            text="like"
+            loadingText="like ..."
           />
         </form>
-        <div style={{ float: "right" }}>
-          <div>
-            {this.state.data.length > 0 ? this.displayResult() : <div />}
-          </div>
-        </div>
       </div>
     );
   }
